82c31076e365579dc63b1910af5a1137846b7896
Вадим Волоткяйвичус <volotkyayvichusvs@sibintek.ru>
2022-03-15 14:01:35 +0700

    Changed useState type.

--- src/hooks/useData.tsx
+++ src/hooks/useData.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import { delay } from 'utils';
 
-export const useData = <T,>(delayMs: number): [T[], boolean] => {
+export const useData = <T,>(delayMs: number): [T | null, boolean] => {
   const [loading, setLoading] = React.useState<boolean>(false);
-  const [data, setData] = React.useState<T[]>([]);
+  const [data, setData] = React.useState<T | null>(null);
 
   React.useEffect(() => {
     const fetchData = async () => {
